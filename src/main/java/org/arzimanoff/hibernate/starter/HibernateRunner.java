6e5fdbe49cfe7c264eb5b6fc852ce79e2f8e366f
ArzimanOff <arzimanovgyulahmed@yandex.ru>
2025-02-22 17:18:43 +0300

    добавил логирование log4j

--- src/main/java/org/arzimanoff/hibernate/starter/HibernateRunner.java
+++ src/main/java/org/arzimanoff/hibernate/starter/HibernateRunner.java
@@ -1,16 +1,17 @@
 package org.arzimanoff.hibernate.starter;
 
-import org.arzimanoff.hibernate.converter.BirthdayConverter;
+import lombok.extern.slf4j.Slf4j;
 import org.arzimanoff.hibernate.entity.Birthday;
 import org.arzimanoff.hibernate.entity.Role;
 import org.arzimanoff.hibernate.entity.User;
 import org.arzimanoff.hibernate.util.HibernateUtil;
-import org.hibernate.cfg.Configuration;
 
 import java.time.LocalDate;
-import java.time.Month;
 
+@Slf4j
 public class HibernateRunner {
+
+
     public static void main(String[] args) {
 
         // Transient
@@ -22,13 +23,18 @@ public class HibernateRunner {
                 .role(Role.USER)
                 .build();
 
+
+        log.info("Объект User создан и является Transient : {}", user);
+
         try (var sessionFactory = HibernateUtil.buildSessionFactory()) {
 
             try(var session1 = sessionFactory.openSession()){
                 session1.beginTransaction();
-
                 // persistent к session1 и transient к session2
+                user.setFirstname("Ivan");
+                log.warn("Объект User был изменен: {}", user);
                 session1.saveOrUpdate(user);
+                log.debug("Объект User был сохранен: {} в сессии {}", user, session1);
                 session1.getTransaction().commit();
             }
 
@@ -37,10 +43,9 @@ public class HibernateRunner {
                 session2.delete(user);
                 session2.getTransaction().commit();
             }
-
-
-
+        } catch (Exception e){
+            log.error("Exception: ", e);
+            throw e;
         }
-
     }
 }
